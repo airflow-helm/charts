fb72810b4351b57f9d0b6eb8f3ec5fa0bde38721
Mathew Wicks <thesuperzapper@users.noreply.github.com>
2022-06-15 07:50:08 +1000

    fix: only set SQLALCHEMY_DATABASE_URI in 1.10
    Signed-off-by: Mathew Wicks <thesuperzapper@users.noreply.github.com>

--- charts/airflow/files/webserver_config.py
+++ charts/airflow/files/webserver_config.py
@@ -1,8 +1,10 @@
-from airflow import configuration as conf
 from flask_appbuilder.security.manager import AUTH_DB
 
-# the SQLAlchemy connection string
-SQLALCHEMY_DATABASE_URI = conf.get('core', 'SQL_ALCHEMY_CONN')
+{{- if .Values.airflow.legacyCommands }}
+# only needed for airflow 1.10
+from airflow import configuration as conf
+SQLALCHEMY_DATABASE_URI = conf.get("core", "SQL_ALCHEMY_CONN")
+{{- end }}
 
 # use embedded DB for auth
 AUTH_TYPE = AUTH_DB
